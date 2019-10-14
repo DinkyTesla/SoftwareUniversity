93bbf1f3b038f47e94799ce62869db6975b391c5
Yavor Rusev <45295249+DinkyTesla@users.noreply.github.com>
2019-10-14 13:52:15 +0300

    Add files via upload

---
+++ First Steps In Coding/04.RectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace _04.RectangleArea
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double SideA = double.Parse(Console.ReadLine());
+            double SideB = double.Parse(Console.ReadLine());
+
+            double area = SideA * SideB;
+
+            Console.WriteLine(area);
+        }
+    }
+}
