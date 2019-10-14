b322ff717e586403f5d2c619ba4bac8b645534ec
Yavor Rusev <45295249+DinkyTesla@users.noreply.github.com>
2019-10-14 14:14:05 +0300

    Add files via upload

---
+++ 1. C#/2018 Tech Fundamentals/2018.06.04 HelloSoftuni/HelloSoftuni/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloSoftuni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 10; row++)
+            {
+                for (int starsCount = 1; starsCount <= row; starsCount++)
+
+                {
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 1. C#/2018 Tech Fundamentals/2018.06.06 simpleCalculations/testHome/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter Your First Name: ");
+
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Last Name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Home Town: ");
+        string homeTown = Console.ReadLine();
+
+        Console.WriteLine("Enter Your Age: ");
+        int age = int.Parse( Console.ReadLine());
+
+        Console.WriteLine("Hey! So, your name is "+firstName + " " + lastName + ", right? And you are from " + homeTown + ". A "
+          + age + " years old .. but wait?"  +" "+ "Male or Female? It doesn't matter." );
+    }
+}
